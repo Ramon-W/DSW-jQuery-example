c293b36c31240b6f28cfe4926c172063433627e7
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-01-28 09:31:11 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
 		$("p").css({"color":"red", "background-color": "yellow"});
 		$("img").css("border","25px solid black");
 	});
+	$("p1").click(function() {
+		$(".disappear").hide();
+	});
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
 	});
